acea8a53b5a339798f17d3857c491bc52f067fad
czjan23 <czjan23@gmail.com>
2019-02-25 18:43:20 -0600

    Parser added

--- 678.py
+++ 678.py
@@ -4,33 +4,54 @@ from time import time
 
 FILE = 'rwtest'
 
-def write_test(file, total_size_mb):
+def write_test(file, size_bytes):
     f = os.open(file, os.O_CREAT | os.O_WRONLY)
-    size_bytes = total_size_mb * 1024 * 1024
     data = os.urandom(size_bytes)
     start_time = time() * 1000
     os.write(f, data)
     os.fsync(f)
     end_time = time() * 1000
     os.close(f)
-    t = end_time - start_time
-    print("Write %d MB in %.2f ms." % (total_size_mb, t))
+    return end_time - start_time
 
-def read_test(file, total_size_mb):
+def read_test(file, size_bytes):
     f = os.open(file, os.O_RDONLY)
-    size_bytes = total_size_mb * 1024 * 1024
     start_time = time() * 1000
     data = os.read(f, size_bytes)
     end_time = time() * 1000
     os.close(f)
-    t = end_time - start_time
-    print("Read %d MB in %.2f ms." % (total_size_mb, t))
+    return end_time - start_time
 
-def test(file, size):
-    write_test(file, size)
-    read_test(file, size)
+def test(file, size, unit):
+    size_bytes = size
+    if unit.lower() == 'kb':
+        size_bytes *= 1024
+    elif unit.lower() == 'mb':
+        size_bytes *= 1024 * 1024
+    elif unit.lower() == 'gb':
+        size_bytes *= 1024 * 1024 * 1024
+
+    print('----------Storage Performance Test----------')
+
+    wt = write_test(file, size_bytes)
+    avg_ws = float(size_bytes) / wt
+    print("Write %d %s in %.2f ms." % (size, unit.upper(), wt))
+    print("Average writing speed: %.2f Bytes / ms" % (avg_ws))
+
+    print('---------------------------------------------------')
+
+    rt = read_test(file, size_bytes)
+    avg_rs = float(size_bytes) / rt
+    print("Read %d %s in %.2f ms." % (size, unit.upper(), rt))
+    print("Average reading speed: %.2f Bytes / ms" % (avg_rs))
+
     os.remove(file)
 
 if __name__ == "__main__":
-    size_mb = int(sys.argv[1])
-    test(FILE, size_mb)
+    if len(sys.argv) != 3:
+        print('Usage error, pls try again.') # parameters check, not that comprehensive
+    else:
+        size = int(sys.argv[1])
+        unit = sys.argv[2]
+
+        test(FILE, size, unit)
