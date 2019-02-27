350e9124f14363c7600578ebe4424787ea6305cd
harry08010 <weitongfeng@hotmail.com>
2019-02-26 20:20:29 -0600

    add default test file size

--- storage_test.py
+++ storage_test.py
@@ -73,10 +73,12 @@ def test(file, size, unit):
     elif unit.lower() == 'gb':
         size_bytes *= 1024 * 1024 * 1024
 
+    result = list()
     print('----------Storage Performance Test----------')
 
     wt = write_test(file, size_bytes)
     avg_ws = float(size_bytes) / wt
+    result.append(avg_ws)
     print("Write %d %s in %.2f ms." % (size, unit.upper(), wt))
     print("Average writing speed: %.2f Bytes / ms" % (avg_ws))
 
@@ -84,12 +86,14 @@ def test(file, size, unit):
 
     rt = read_test(file, size_bytes)
     avg_rs = float(size_bytes) / rt
+    result.append(avg_rs)
     print("Read %d %s in %.2f ms." % (size, unit.upper(), rt))
     print("Average reading speed: %.2f Bytes / ms" % (avg_rs))
 
     print('----------------End Of Test-----------------')
 
     os.remove(file)
+    return result
 
 if __name__ == "__main__":
     """First do a parameters check. If the parameters are valid, do read/write test
@@ -106,7 +110,26 @@ if __name__ == "__main__":
     """
 
     if len(sys.argv) != 3 or sys.argv[2].lower() not in ['b', 'kb', 'mb', 'gb']: # parameters check
-        print('Usage error, pls try again.')
+        # print('Usage error, pls try again.')
+        # print('Usage: python [file_name].py [size] [unit]')
+        # print('Example: python storage_test.py 10 kb')
+        print('Running default tests:')
+        size_list = [1, 128, 256, 512, 1024, 1, 128, 256, 512, 1024, 2]
+        unit_list = ['kb', 'kb', 'kb', 'kb', 'kb', 'mb', 'mb', 'mb', 'mb', 'mb', 'gb']
+        ws_result_list = list()
+        rs_result_list = list()
+        for i in range(10):
+            print('Processing:' + str(size_list[i]) + str(unit_list[i]))
+            test_result = test(FILE, size_list[i], unit_list[i])
+            ws_result_list.append(test_result[0])
+            rs_result_list.append(test_result[1])
+        
+        avg_ws_default = sum(ws_result_list) / float(len(ws_result_list))
+        avg_rs_default = sum(rs_result_list) / float(len(rs_result_list))
+
+        print("Average writing speed: %.2f Bytes / ms" % (avg_ws_default))
+        print("Average reading speed: %.2f Bytes / ms" % (avg_rs_default))
+        print('Note: You can also set the file size yourself.')
         print('Usage: python [file_name].py [size] [unit]')
         print('Example: python storage_test.py 10 kb')
     else:
